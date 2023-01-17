f398d140f702e37e94780d94f1acd7c1ad155d7e
Kyu Lee <kwooler@gmail.com>
2023-01-17 09:30:28 -0800

    L34

--- main.py
+++ main.py
@@ -2,21 +2,6 @@ def main():
     ##################################################
     # Comlete your code here
     ##################################################
-    num1 = int(input('Enter the number 1'))
-    num2 = int(input('Enter the number 2'))
-    num3 = int(input('Enter the number 3'))
-
-    if (num1 > num2):
-        if (num1 > num3):
-            print("The greatest number is", num1)
-        else:
-            print("The greatest number is", num3)
-    else:
-        if (num2 > num3):
-            print("The greatest number is", num2)
-        else:
-            print("The greatest number is", num3)
-
     pass
 
 
