94690c3c2acf6644b0667399147a16fbf55c2775
Kyu Lee <kwooler@gmail.com>
2023-05-21 20:50:13 -0700

    Lab 3-4

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 def main():
-    ##################################################
-    # Comlete your code here
-    ##################################################
-    pass
+    x = int(input('Enter your input: '))
+    y = int(input('Enter your input: '))
+    """
+    ########################################
+    Code Your Program here
+    ########################################
+    """
+
+    print(f'Quadrant: {quadrant}')
+    ########################################
+    # Do not delete the return statement
+    ########################################
+    return quadrant
 
 
 if __name__ == '__main__':
--- main_test.py
+++ main_test.py
@@ -10,15 +10,16 @@ def test_main_1():
     datastr = '-10\n2\n'
     sys.stdin = io.StringIO(datastr)
 
-    main.main()
+    ret = main.main()
     sys.stdout = sys.__stdout__
     print('Captured ', captureOut.getvalue())
     lines = captureOut.getvalue().split('\n')
     print(lines)
 
-    res = re.search(r'[\w,\W]*uadrant 2[\w,\W]*', lines[0])
-    assert res != None
-    print(res.group())
+    assert ret == 2
+    # res = re.search(r'[\w,\W]*uadrant 2[\w,\W]*', lines[0])
+    # assert res != None
+    # print(res.group())
 
 
 def test_main_2():
@@ -27,15 +28,13 @@ def test_main_2():
     datastr = '2\n-10\n'
     sys.stdin = io.StringIO(datastr)
 
-    main.main()
+    ret = main.main()
     sys.stdout = sys.__stdout__
     print('Captured ', captureOut.getvalue())
     lines = captureOut.getvalue().split('\n')
     print(lines)
 
-    res = re.search(r'[\w,\W]*uadrant 4[\w,\W]*', lines[0])
-    assert res != None
-    print(res.group())
+    assert ret == 4
 
 
 def test_main_3():
@@ -44,15 +43,13 @@ def test_main_3():
     datastr = '-10\n-10\n'
     sys.stdin = io.StringIO(datastr)
 
-    main.main()
+    ret = main.main()
     sys.stdout = sys.__stdout__
     print('Captured ', captureOut.getvalue())
     lines = captureOut.getvalue().split('\n')
     print(lines)
 
-    res = re.search(r'[\w,\W]*Quadrant 3[\w,\W]*', lines[0])
-    assert res != None
-    print(res.group())
+    assert ret == 3
 
 
 def test_main_4():
@@ -61,12 +58,10 @@ def test_main_4():
     datastr = '10\n10\n'
     sys.stdin = io.StringIO(datastr)
 
-    main.main()
+    ret = main.main()
     sys.stdout = sys.__stdout__
     print('Captured ', captureOut.getvalue())
     lines = captureOut.getvalue().split('\n')
     print(lines)
 
-    res = re.search(r'[\w,\W]*Quadrant 1[\w,\W]*', lines[0])
-    assert res != None
-    print(res.group())
+    assert ret == 1
